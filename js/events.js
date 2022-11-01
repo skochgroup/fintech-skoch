b3c6671d8ee743a0f30b17fb2bb91e14b41b23a1
skochgroup <admin@skoch.in>
2022-11-01 15:19:11 +0530

    first commit

---
+++ js/events.js
@@ -0,0 +1,3 @@
+var canvasEvents = {
+	'10-11-2022' : [{content: '<a href="#" target=_blank></a>', startTime: '06:30 ', endTime: ' 16:15'},{content: '<a href="#" target=_blank></a>', startTime: '06:30 ', endTime: ' 07:15'}],
+};
